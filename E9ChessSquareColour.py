6621180f56c45be149376d12c84cc8c3a2b4ac2b
mleewo28 <mikileewo@gmail.com>
2025-07-02 14:40:02 +0000

    row and column logic

---
+++ E9ChessSquareColour.py
@@ -0,0 +1,20 @@
+def getChessSquareColour(column,row): 
+    
+    for row in range(0,8):
+        for column in range(0,8): 
+            if (row % 2 == 0 and column % 2 == 0) or (row % 2 == 1 and column % 2 == 1): 
+                return 'white'
+            else: 
+                return 'black'
+            
+assert getChessSquareColour(0, 0) == 'white'
+
+assert getChessSquareColour(1, 0) == 'black'
+
+assert getChessSquareColour(0, 1) == 'black'
+
+assert getChessSquareColour(7, 7) == 'white'
+
+assert getChessSquareColour(0, 8) == ''
+
+assert getChessSquareColour(2, 9) == ''
