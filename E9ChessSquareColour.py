963146d89f32f04c9974391688d5b281fd4a73b8
mleewo28 <mikileewo@gmail.com>
2025-07-03 04:34:31 +0000

    removed loops for function as variables were being overwritten

--- E9ChessSquareColour.py
+++ E9ChessSquareColour.py
@@ -1,11 +1,12 @@
 def getChessSquareColour(column,row): 
     
-    for row in range(0,8):
-        for column in range(0,8): 
-            if (row % 2 == 0 and column % 2 == 0) or (row % 2 == 1 and column % 2 == 1): 
-                return 'white'
-            else: 
-                return 'black'
+    #Account for arguments outside of the 0 to 7 range for 'row' and 'column'
+    if row > 7 or row < 0 or column > 7 or column < 0:
+        return ''
+    elif (row % 2 == 0 and column % 2 == 0) or (row % 2 == 1 and column % 2 == 1): 
+        return 'white'
+    else: 
+        return 'black'
             
 assert getChessSquareColour(0, 0) == 'white'
 
