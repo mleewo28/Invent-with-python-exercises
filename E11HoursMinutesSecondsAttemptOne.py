b16397a59750bb9f4be0544f467dc5b2488a9946
mleewo28 <mikileewo@gmail.com>
2025-07-05 16:42:19 +0000

    Accounted for 0 variables for hours, mins and secs

--- E11HoursMinutesSecondsAttemptOne.py
+++ E11HoursMinutesSecondsAttemptOne.py
@@ -4,15 +4,19 @@ def getHoursMinutesSeconds(totalSeconds):
     hours = 0 
     mins = 0 
     seconds = 0
+
+    if totalSeconds ==0: 
+        return '0s'
+    
     while remainder > 0: 
 
-        if remainder > 3600: 
+        if remainder >= 3600: 
             hours = remainder//3600 
             wholeHrinSec = hours*60*60 
             remainder -= wholeHrinSec
             #print(f"in if {remainder}")
 
-        elif remainder > 60: 
+        elif remainder >= 60: 
             mins = remainder//60 
             wholeMinInSec = mins*60 
             remainder -= wholeMinInSec
@@ -22,7 +26,32 @@ def getHoursMinutesSeconds(totalSeconds):
             seconds = remainder
             remainder = 0 
 
-    return f"{hours}h {mins}m {seconds}s"
+    conversion = '' 
+    if hours != 0: 
+        conversion += str(hours) + 'h '
+    if mins != 0: 
+        conversion += str(mins) + 'm '
+    if seconds != 0: 
+        conversion += str(seconds) + 's'
+    
+    return conversion.strip() 
+ 
 
 
 assert getHoursMinutesSeconds(3661) == '1h 1m 1s'
+
+assert getHoursMinutesSeconds(30) == '30s'
+
+assert getHoursMinutesSeconds(60) == '1m'
+
+assert getHoursMinutesSeconds(90) == '1m 30s'
+
+assert getHoursMinutesSeconds(3600) == '1h'
+
+assert getHoursMinutesSeconds(3601) == '1h 1s'
+
+assert getHoursMinutesSeconds(3661) == '1h 1m 1s'
+
+assert getHoursMinutesSeconds(90042) == '25h 42s'
+
+assert getHoursMinutesSeconds(0) == '0s'
