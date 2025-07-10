fe0ebfad01076640d9d58dd4c04974f44d1e0aa8
mleewo28 <mikileewo@gmail.com>
2025-07-10 23:56:04 +0000

    fixed the spacing and alignment

--- E25MultiplicationTable.py
+++ E25MultiplicationTable.py
@@ -1,11 +1,11 @@
 def printMultiplicationTable():
-    print(' | 1 2 3 4 5 6 7 8 9 10')
-    print('-+----------------------')
+    print(' | 1  2  3  4  5  6  7  8  9  10')
+    print('--+-----------------------------')
     for row in range(1,11):
-        print(f"{row}| ", end='')
+        print(f"{row}".rjust(2)+'|', end='')
         for column in range(1,11):
             product = column*row 
-            print(f"{product} ", end='')
+            print(f"{product}".rjust(2) +" ",end='')
         print() #print a new line after every row 
 
 
