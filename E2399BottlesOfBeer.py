e2d511e1ecda444aa7765978c34aa3854c51a4a1
mleewo28 <mikileewo@gmail.com>
2025-07-10 17:04:11 +0000

    print the song 99 bottles of beer on the wall

---
+++ E2399BottlesOfBeer.py
@@ -0,0 +1,16 @@
+def main():
+
+    for i in range(99,2,-1):
+        print(f"{i} bottles of beer on the wall,\n"
+              +f"{i} bottles of beer\n"
+              +f"Take one down,\n"
+              +f"Pass it around,\n"
+              +f"{i-1} bottles of beer on the wall,\n")
+        
+    print("1 bottle of beer on the wall,\n"
+          +"1 bottle of beer,\n"
+          +"Take one down,\n"
+          +"Pass it around,\n"
+          +"No more bottles of beer on the wall!\n")
+
+main() 
