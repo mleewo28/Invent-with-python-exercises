5547ac86467806450a839f30101c11f63c04085b
mleewo28 <mikileewo@gmail.com>
2025-07-17 22:47:59 +0000

    attempt to draw a pyramid

---
+++ E29PyramidDrawing.py
@@ -0,0 +1,14 @@
+def drawPyramid(height):
+    row = 1 
+    while height > 0:
+
+        for i in range(height - row):
+            print(' ', end = '')
+
+        for j in range(2*row-1):
+            print('#', end = '')
+        print() 
+        height -= 1 
+        row += 1 
+
+drawPyramid(5)
