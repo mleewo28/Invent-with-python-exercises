05d63c2e555504ff6d1589735de577c5ee59add8
mleewo28 <mikileewo@gmail.com>
2025-07-17 03:27:27 +0000

    wrote a function to print a rectangle of any height and width

---
+++ E27RectangleDrawing.py
@@ -0,0 +1,11 @@
+def drawRectangle(width, height): 
+
+    for i in range(height):
+        for j in range(width):
+            print("#", end = '')
+
+        print() 
+
+drawRectangle(4,5)
+drawRectangle(48,56)
+drawRectangle(1,2)
