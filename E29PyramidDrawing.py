4ab43c74effe589764417c8ebfee8bb5beaba8db
mleewo28 <mikileewo@gmail.com>
2025-07-18 02:39:19 +0000

    centred the pyramid

--- E29PyramidDrawing.py
+++ E29PyramidDrawing.py
@@ -1,16 +1,16 @@
 def drawPyramid(height):
     row = 1 
-    while height > 0:
 
+    while row < height: 
         for i in range(height - row):
             print(' ', end = '')
 
         for j in range(2*row-1):
             print('#', end = '')
         print() 
-        height -= 1 
+        #height -= 1 
         row += 1 
 
-drawPyramid(5)
-drawPyramid(2)
-drawPyramid(10)
+drawPyramid(8)
+# drawPyramid(2)
+# drawPyramid(10)
