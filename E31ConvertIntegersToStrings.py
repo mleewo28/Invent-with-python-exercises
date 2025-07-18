369afaae8e00d18c13b167bc102af2b02f8226fb
mleewo28 <mikileewo@gmail.com>
2025-07-18 00:37:17 +0000

    implemented a convert to integer function, come back to the error thrown in the while loop

--- E31ConvertIntegersToStrings.py
+++ E31ConvertIntegersToStrings.py
@@ -1,13 +1,22 @@
-def convertIntTo(integerNum):
+def convertIntToStr(integerNum):
     if integerNum == 0: 
         return '0'
+    
+    isNegative = integerNum < 0 
     
     FROM_digits_To_Str = {0:'0',1:'1',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9'}
     strNum = ''
 
-    while integerNum > 0:
+    while abs(integerNum) > 0:
         ones_digit = integerNum % 10 
         strNum = FROM_digits_To_Str[ones_digit] + strNum
         integerNum = integerNum//10 
     
+    if isNegative:
+        return '-' +strNum
+    
     return strNum 
+
+for i in range(-10000,10000):
+
+    assert convertIntToStr(i) == str(i)
