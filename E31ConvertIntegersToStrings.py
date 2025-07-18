0468205a5ef8b90da44b0cb2f6720fb5f461b6e3
mleewo28 <mikileewo@gmail.com>
2025-07-18 03:01:55 +0000

    testing negative values

--- E31ConvertIntegersToStrings.py
+++ E31ConvertIntegersToStrings.py
@@ -4,18 +4,26 @@ def convertIntToStr(integerNum):
     
     isNegative = integerNum < 0 
     
+    
     FROM_digits_To_Str = {0:'0',1:'1',2:'2',3:'3',4:'4',5:'5',6:'6',7:'7',8:'8',9:'9'}
     strNum = ''
 
-    while abs(integerNum) > 0:
+    integerNum = abs(integerNum)
+
+    while integerNum > 0:
         ones_digit = integerNum % 10 
+        #print(integerNum)
         strNum = FROM_digits_To_Str[ones_digit] + strNum
         integerNum = integerNum//10 
     
     if isNegative:
-        return '-' +strNum
+        return '-' + strNum
     
+    print(strNum)
+
     return strNum 
+
+convertIntToStr(-1000)
 
 for i in range(-10000,10000):
 
