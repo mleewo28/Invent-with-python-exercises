df6ae6167146fff58b95378cce60c2622c9d1808
mleewo28 <mikileewo@gmail.com>
2025-07-24 03:12:58 +0000

    wrote a title case function

---
+++ E35TitleCase.py
@@ -0,0 +1,49 @@
+def getTitleCase(text):
+
+    if text == '':
+        return ''
+
+    title_case = ''
+    title_case = text[0].upper() 
+
+    for i in range(1,len(text)):
+        if text[i-1].isalpha():
+            title_case += text[i].lower() 
+
+        else: 
+            title_case+= text[i].upper() 
+
+    return title_case
+
+
+assert getTitleCase('Hello, world!') == 'Hello, World!'
+
+assert getTitleCase('HELLO') == 'Hello'
+
+assert getTitleCase('hello') == 'Hello'
+
+assert getTitleCase('hElLo') == 'Hello'
+
+assert getTitleCase('') == ''
+
+assert getTitleCase('abc123xyz') == 'Abc123Xyz'
+
+assert getTitleCase('cat dog RAT') == 'Cat Dog Rat'
+
+assert getTitleCase('cat,dog,RAT') == 'Cat,Dog,Rat'
+
+ 
+
+import random
+
+random.seed(42)
+
+chars = list('abcdefghijklmnopqrstuvwxyz1234567890 ,.')
+
+for i in range(1000):
+
+    random.shuffle(chars)
+
+    assert getTitleCase(''.join(chars)) == ''.join(chars).title()
+
+
