9b6c54ea0b7b057efb523b2408852abee24a27df
mleewo28 <mikileewo@gmail.com>
2025-07-30 20:56:10 +0000

    wrote function for the collatz sequence. "append" object not working as it should. Working to debug the issue

---
+++ E39CollatzSequence.py
@@ -0,0 +1,48 @@
+def collatz(startingNumber):
+
+    sequence = []
+
+    #check if startingNumber is less than 1 
+    if startingNumber < 1:
+        return [] 
+    else: 
+        sequence = sequence.append(startingNumber)
+
+    #create the sequence using even and odd properties 
+    while startingNumber > 1: 
+        if startingNumber % 2 ==0: 
+            startingNumber = startingNumber//2 
+            sequence = sequence.append(startingNumber)
+        else: 
+            startingNumber = (3*startingNumber) + 1 
+            sequence = sequence.append(startingNumber)
+
+    return sequence
+
+
+
+assert collatz(0) == []
+
+assert collatz(10) == [10, 5, 16, 8, 4, 2, 1]
+
+assert collatz(11) == [11, 34, 17, 52, 26, 13, 40, 20, 10, 5, 16, 8, 4, 2, 1]
+
+assert collatz(12) == [12, 6, 3, 10, 5, 16, 8, 4, 2, 1]
+
+assert len(collatz(256)) == 9
+
+assert len(collatz(257)) == 123
+
+import random
+
+random.seed(42)
+
+for i in range(1000):
+
+    startingNum = random.randint(1, 10000)
+
+    seq = collatz(startingNum)
+
+    assert seq[0] == startingNum # Make sure it includes the starting number.
+
+    assert seq[-1] == 1  # Make sure the last integer is 1.
