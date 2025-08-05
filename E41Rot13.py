cd03845974bf72a1a0a1a5d2e9a25c34bfe695af
mleewo28 <mikileewo@gmail.com>
2025-08-05 23:55:30 +0000

    wrote a part of the ROT encrypted function

---
+++ E41Rot13.py
@@ -0,0 +1,32 @@
+def rot13(text): 
+    encrypted_text = '' 
+
+    for char in text: 
+        if char.isalpha(): 
+            if char.islower():
+                if ord('char') <= 109: 
+                    encrypted_text += chr(ord('char') +13) 
+                else: 
+                    encrypted_text += chr(ord('char') - 13)
+            else: 
+                #the character is uppercase 
+                if ord('char') <= 77:
+                    encrypted_text += chr(ord('char') +13)
+                else: 
+                    encrypted_text += chr(ord('char') -13)
+        else: 
+            encrypted_text += 'char'
+
+
+assert rot13('Hello, world!') == 'Uryyb, jbeyq!'
+
+assert rot13('Uryyb, jbeyq!') == 'Hello, world!'
+
+assert rot13(rot13('Hello, world!')) == 'Hello, world!'
+
+assert rot13('abcdefghijklmnopqrstuvwxyz') == 'nopqrstuvwxyzabcdefghijklm'
+
+assert rot13('ABCDEFGHIJKLMNOPQRSTUVWXYZ') == 'NOPQRSTUVWXYZABCDEFGHIJKLM'
+
+
+
