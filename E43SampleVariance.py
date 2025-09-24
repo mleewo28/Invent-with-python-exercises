09971a5583576523eca649e31d473146c7d9905a
mleewo28 <mikileewo@gmail.com>
2025-09-24 01:09:50 +0000

    wrote a variance sum function

---
+++ E43SampleVariance.py
@@ -0,0 +1,10 @@
+def sample_variance(numbers): 
+    variance_sum = 0 
+    for i in numbers: 
+        variance_sum += (i-(sum(numbers)/len(numbers)))**2
+
+    sample_variance = variance_sum/(len(numbers)-1)
+    
+    return sample_variance
+
+print(sample_variance([3.4,2.5,4.8,2.9,3.6,2.8,3.3,5.6,3.7,2.8,4.4,4.0,5.2,3.0,4.8]))
