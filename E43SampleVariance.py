87b70a87274253b9b9bd6fa3af7e1eb897bade45
mleewo28 <mikileewo@gmail.com>
2025-09-24 01:54:20 +0000

    print statements

--- E43SampleVariance.py
+++ E43SampleVariance.py
@@ -7,4 +7,6 @@ def sample_variance(numbers):
     
     return sample_variance
 
-print(sample_variance([3.4,2.5,4.8,2.9,3.6,2.8,3.3,5.6,3.7,2.8,4.4,4.0,5.2,3.0,4.8]))
+#print(sample_variance([3.4,2.5,4.8,2.9,3.6,2.8,3.3,5.6,3.7,2.8,4.4,4.0,5.2,3.0,4.8]))
+#print(sample_variance([227,222,218,217,225,218,216,229,228,221]))
+print(sample_variance([]))
